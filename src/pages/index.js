e61db68de93ed4a6dc7e254c552efc21cba27b3f
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-12-01 23:24:55 +0100

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+	fetch('.netlify/fn/foo', {
+		method: 'GET',
+		headers: {
+			'X-foo': 'fooX',
+		}
+	}).then(data => console.log(data))
+	return <div>Hello world!</div>
 }
